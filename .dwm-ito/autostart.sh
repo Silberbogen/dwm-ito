5fff2f4e29b3d586fab49a1c7e01c76d57a67e07
Sascha Biermanns <skkd.h4k1n9@yahoo.de>
2016-06-18 23:14:40 +0200

    Anpassung der Dateien an Debian Jessie + aktuelle Backports

--- .dwm-ito/autostart.sh
+++ .dwm-ito/autostart.sh
@@ -1,17 +1,11 @@
 #!/bin/bash
 #eval $(gnome-keyring-daemon -s --components=pkcs11,secrets,ssh,gpg) &
-#synclient VertEdgeScrol=1 &
 syndaemon -t -k -i 2 -d &
 synclient TapButton1=1 &
-#synclient TapButton2=2 &
-#synclient TapButton3=3 &
-#synclient touchpadoff=1 &
-#setxkbmap -layout de nodeadkeys &
 conky -c conkydwm-ito | while read -r; do xsetroot -name "$REPLY"; done &
 nitrogen --restore &
 xfce4-volumed &
 xfce4-power-manager &
-#xscreensaver -no-splash &
 xss-lock -- slock &
 nm-applet &
 unclutter &

ac5d44f1f5994c4230175ebe7f7f5b8b4ae7410f
Sascha Biermanns <skkd.h4k1n9@yahoo.de>
2017-01-10 04:17:27 +0100

    Änderungen für das aktuelle Jessie-System

--- .dwm-ito/autostart.sh
+++ .dwm-ito/autostart.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
-#eval $(gnome-keyring-daemon -s --components=pkcs11,secrets,ssh,gpg) &
+eval $(gnome-keyring-daemon -s --components=pkcs11,secrets,ssh,gpg) &
 syndaemon -t -k -i 2 -d &
 synclient TapButton1=1 &
-conky -c conkydwm-ito | while read -r; do xsetroot -name "$REPLY"; done &
+conky -c ~/.dwm-ito/conkydwm-ito | while read -r; do xsetroot -name "$REPLY"; done &
 nitrogen --restore &
 xfce4-volumed &
 xfce4-power-manager &
-xss-lock -- slock &
+# Enable one of the two following screensave-methods
+# xss-lock -- slock &
+# xscreensaver -no-splash &
 nm-applet &
 unclutter &
 

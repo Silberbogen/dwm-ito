e8f951ae1f1fa42df2e6c708df5ee34f96295e8e
Sascha Biermanns <skkd.h4k1n9@yahoo.de>
2017-01-10 11:02:01 +0100

    Aufräumvorgang verbessert und autostop.sh eingeführt

---
+++ .dwm-ito/autostop.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+pkill synclient
+pkill syndaemon
+pkill conky
+pkill nitrogen
+pkill xfce4-volumed
+pkill xfce4-power-manager
+# Disable if activated
+# pkill xss-lock
+# pkill xscreensaver
+pkill nm-applet
+pkill unclutter
+
